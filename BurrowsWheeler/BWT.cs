9320cf185f41023e223e733a8e8877bb9d63a65a
Ivan Glazunov <vanya.glazunov05@gmail.com>
2024-02-25 01:39:48 +0300

    add task Burrows-Wheeler

---
+++ BurrowsWheeler/BWT.cs
@@ -0,0 +1,106 @@
+﻿using System.Text;
+
+namespace BurrowsWheeler;
+
+public static class BWT
+{
+    public static (string, int) Transform(string? inputString)
+    {
+        if (inputString is null) throw new ArgumentException("inputString cannot be null");
+
+        int[] suffixArray = CreateSuffixArray(inputString);
+        int endOfString = 0;
+        StringBuilder transformedString = new(inputString.Length);
+        for (int i = 0; i < suffixArray.Length; ++i)
+        {
+            if (suffixArray[i] != 0)
+            {
+                transformedString.Append(inputString[suffixArray[i] - 1]);
+            }
+            else
+            {
+                transformedString.Append(inputString[^1]);
+                endOfString = i;
+            }
+        }
+
+        return (transformedString.ToString(), endOfString);
+    }
+
+    public static string InverseTransform(string? transformedString, int endOfString)
+    {
+        if (transformedString is null) throw new ArgumentException("transformedString cannot be null");
+        if (endOfString >= transformedString.Length || endOfString < 0) throw new ArgumentException("endOfString index out of range");
+
+        int[] positions = new int[transformedString.Length];
+        for (int i = 0; i < positions.Length; ++i) positions[i] = i;
+        char[] transformedStringChars = transformedString.ToCharArray();
+        Array.Sort(transformedStringChars, positions);
+
+        StringBuilder orginalString = new(transformedString.Length);
+        int position = endOfString;
+        for (int i = 0; i < transformedString.Length; ++i)
+        {
+            orginalString.Append(transformedStringChars[position]);
+            position = positions[position];
+        }
+
+        return orginalString.ToString();
+    }
+
+    private static int[] CreateSuffixArray(string inputString)
+    {
+        int[] suffixArray = new int[inputString.Length];
+        int[] equivalenceClasses = new int[inputString.Length];
+
+        for (int i = 0; i < suffixArray.Length; ++i) suffixArray[i] = i;
+
+        Array.Sort(inputString.ToCharArray(), suffixArray);
+
+        for (int i = 1; i < suffixArray.Length; ++i)
+        {
+            equivalenceClasses[suffixArray[i]] = inputString[suffixArray[i]] == inputString[suffixArray[i - 1]] ?
+            equivalenceClasses[suffixArray[i - 1]] : equivalenceClasses[suffixArray[i - 1]] + 1;
+        }
+
+        for (int i = 0; (1 << i) < inputString.Length; ++i)
+        {
+            for (int j = 0; j < suffixArray.Length; ++j)
+            {
+                suffixArray[j] = (suffixArray[j] - (1 << i) + suffixArray.Length) % suffixArray.Length;
+            }
+
+            suffixArray = CountSort(suffixArray, equivalenceClasses);
+
+            int[] newEquivalenceClasses = new int[equivalenceClasses.Length];
+
+            for (int j = 1; j < suffixArray.Length; ++j)
+            {
+                (int, int) previousBlock = new(equivalenceClasses[suffixArray[j - 1]], equivalenceClasses[(suffixArray[j - 1] + (1 << i)) % suffixArray.Length]);
+                (int, int) currentBlock = new(equivalenceClasses[suffixArray[j]], equivalenceClasses[(suffixArray[j] + (1 << i)) % suffixArray.Length]);
+                newEquivalenceClasses[suffixArray[j]] = previousBlock == currentBlock ? newEquivalenceClasses[suffixArray[j - 1]] : newEquivalenceClasses[suffixArray[j - 1]] + 1;
+            }
+
+            equivalenceClasses = newEquivalenceClasses;
+        }
+
+        return suffixArray;
+    }
+
+    private static int[] CountSort(int[] suffixArray, int[] equivalenceClasses)
+    {
+        int[] count = new int[suffixArray.Length];
+        int[] positions = new int[suffixArray.Length];
+        foreach (int element in equivalenceClasses) count[element]++;
+        for (int i = 1; i < suffixArray.Length; ++i) positions[i] = positions[i - 1] + count[i - 1];
+        int[] sortedArray = new int[suffixArray.Length];
+        foreach (int element in suffixArray)
+        {
+            int elementClass = equivalenceClasses[element];
+            sortedArray[positions[elementClass]] = element;
+            positions[elementClass]++;
+        }
+
+        return sortedArray;
+    }
+}
---
+++ BurrowsWheeler/Program.cs
@@ -0,0 +1,79 @@
+﻿using BurrowsWheeler;
+
+bool TestBWT(string input, (string, int) output)
+{
+    return BWT.Transform(input) == output;
+}
+
+bool TestInverseBWT((string, int) input, string output)
+{
+    return BWT.InverseTransform(input.Item1, input.Item2) == output;
+}
+
+string[] originalStrings = new[] { "BANANA", "A", "AA BB$" };
+(string, int)[] transform = new[] { ("NNBAAA", 3), ("A", 0), ("ABA$B ", 3) };
+bool testsPassed = true;
+for (int test = 0; test < 3; ++test)
+{
+    if (!TestBWT(originalStrings[test], transform[test]))
+    {
+        Console.WriteLine($"Burrows-Wheeler Transform failed with string: {originalStrings[test]}");
+        testsPassed = false;
+    }
+    if (!TestInverseBWT(transform[test], originalStrings[test]))
+    {
+        Console.WriteLine($"Inverse Burrows-Wheeler Transform failed with: {transform[test].Item1}, {transform[test].Item2}");
+        testsPassed = false;
+    }
+}
+
+if (!testsPassed)
+{
+    return;
+}
+else
+{
+    Console.WriteLine("All tests passed");
+}
+
+
+bool quit = false;
+while (!quit)
+{
+    Console.WriteLine("choose comand:\n1. Burrows-Wheeler Transform\n2. Inverse Burrows-Wheeler Transform\n3. Quit");
+    string? comand = Console.ReadLine();
+    switch (comand)
+    {
+        case "1":
+            Console.WriteLine("Enter string");
+            string? inputString = Console.ReadLine();
+            (string, int) Transform = BWT.Transform(inputString);
+            Console.WriteLine($"Transformed string: {Transform.Item1}, the end of the string position(zero-based): {Transform.Item2}");
+            break;
+        case "2":
+            Console.WriteLine("Enter string and the end of the string position(zero-based)");
+            inputString = Console.ReadLine();
+            if (inputString is null)
+            {
+                Console.WriteLine("Incorrect input");
+                break;
+            }
+
+            string[] arguments = inputString.Split();
+
+            if (arguments.Length != 2 || !int.TryParse(arguments[1], out int endOfString))
+            {
+                Console.WriteLine("Incorrect input");
+                break;
+            }
+
+            Console.WriteLine($"Original string: {BWT.InverseTransform(arguments[0], endOfString)}");
+            break;
+        case "3":
+            quit = true;
+            break;
+        default:
+            Console.WriteLine("Incorrect comand");
+            break;
+    }
+}

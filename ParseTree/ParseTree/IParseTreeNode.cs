b7a97b855d6beb3e72957e174eed4d4678078563
Ivan Glazunov <vanya.glazunov05@gmail.com>
2024-03-29 01:14:26 +0300

    Add task ParseTree

---
+++ ParseTree/ParseTree/IParseTreeNode.cs
@@ -0,0 +1,19 @@
+namespace ParseTree;
+
+/// <summary>
+/// Interface of the node in ParseTree.
+/// </summary>
+public interface IParseTreeNode
+{
+    /// <summary>
+    /// Evaluates subtree that is contained by the node.
+    /// </summary>
+    /// <returns>Returns result of the evaluation.</returns>
+    public double Evaluate();
+
+    /// <summary>
+    /// Prints content of the node.
+    /// </summary>
+    /// <returns>Returns string representing node.</returns>
+    public string Print();
+}
---
+++ ParseTree/ParseTree/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter path to the file containing expression");
+
+var filePath = Console.ReadLine();
+
+if (filePath is null)
+{
+    Console.WriteLine($"filePath cannot be null");
+    return;
+}
+
+var expression = File.ReadAllText(filePath);
+
+ParseTree.ParseTree parseTree = new ();
+
+Console.WriteLine($"{parseTree.Print()} = {parseTree.Evaluate()}");

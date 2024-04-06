26fedfa17bd4cb58b1381f20761b8b9e06278f58
Ivan Glazunov <vanya.glazunov05@gmail.com>
2024-04-06 20:55:34 +0300

    Feat: Add task Functions

---
+++ Functions/Functions/Functions.cs
@@ -0,0 +1,39 @@
+﻿namespace Functions;
+
+public static class Functions
+{
+    public static List<TNewValue> Map<TOldValue, TNewValue>(List<TOldValue> list, Func<TOldValue, TNewValue> func)
+    {
+        List<TNewValue> newList = new ();
+        foreach (var item in list)
+        {
+            newList.Add(func(item));
+        }
+
+        return newList;
+    }
+
+    public static List<T> Filter<T>(List<T> list, Func<T, bool> predicate)
+    {
+        List<T> newList = new ();
+        foreach (var item in list)
+        {
+            if (predicate(item))
+            {
+                newList.Add(item);
+            }
+        }
+
+        return newList;
+    }
+
+    public static T Fold<T>(List<T> list, T initialValue, Func<T, T, T> func)
+    {
+        foreach (var item in list)
+        {
+            initialValue = func(initialValue, item);
+        }
+
+        return initialValue;
+    }
+}
